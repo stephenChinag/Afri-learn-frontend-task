2577987420509594d9e742a80bcbf2789413b20c
stephenChinag <stephenchinag@gmail.com>
2024-09-27 11:31:52 +0100

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <h1> Homepage</h1>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to /admin/static when the component loads
+    router.push("/admin/statistic");
+  }, [router]);
+
+  return null; // Optionally, render nothing or a loading spinner while redirecting
 }
